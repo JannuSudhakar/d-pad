b747ca327a8f69252aa1c2ea1497a5fdb51e06b2
Jannu Sudhakar <sudhajannu95@gmail.com>
2021-01-17 05:56:59 +0530

    some logging added to the generateUserStamp function

--- utils/meta-data-functions.js
+++ utils/meta-data-functions.js
@@ -1,4 +1,5 @@
 function generateUserStamp(req){
+  console.log("edit by", req.connection.remoteAddress);
   return {
     name: "not-yet-implemented",
     "ip-address": req.connection.remoteAddress
